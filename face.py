274dea521cd8fee29fac4f09e355916a2efc0747
Jiamu Liu <jiamu421@ucla.edu>
2022-02-24 10:58:00 -0500

    upd ai

--- face.py
+++ face.py
@@ -4,18 +4,20 @@ import imutils
 UPLOAD_FOLDER = 'static/uploads/'
 DEST_FOLDER = 'static/faces/'
 
-face_cas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
-face_img = cv2.imread(UPLOAD_FOLDER + 'face.jpg')
-face_img = imutils.resize(face_img, width=580)
 
-# grayscale img
-face_grey_scale = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
-# detect face
-face_detected = face_cas.detectMultiScale(face_grey_scale, 1.1, 4)
+def faceDetector():
+    face_cas = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
+    face_img = cv2.imread(UPLOAD_FOLDER + 'face.jpg')
+    face_img = imutils.resize(face_img, width=580)
 
-# process img
-for (x, y, w, h) in face_detected:
-    cv2.rectangle(face_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+    # grayscale img
+    face_grey_scale = cv2.cvtColor(face_img, cv2.COLOR_BGR2GRAY)
+    # detect face
+    face_detected = face_cas.detectMultiScale(face_grey_scale, 1.1, 4)
 
-# write result
-cv2.imwrite(DEST_FOLDER + "result.png", face_img)
+    # process img
+    for (x, y, w, h) in face_detected:
+        cv2.rectangle(face_img, (x, y), (x+w, y+h), (255, 0, 0), 2)
+
+    # write result
+    cv2.imwrite(DEST_FOLDER + "face.jpg", face_img)
